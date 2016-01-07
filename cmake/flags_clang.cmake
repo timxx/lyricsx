8ffdf7d14bdad0cf22bc288fd8f6df629e15e85a
Weitian Leung <weitianleung@gmail.com>
2016-01-07 21:28:21 +0800

    use CMAKE_CXX_COMPILER_VERSION detect compiler version

--- cmake/flags_clang.cmake
+++ cmake/flags_clang.cmake
@@ -1,7 +1,5 @@
 if (LRCX_ENABLE_ASAN)
-	execute_process(COMMAND ${CMAKE_CXX_COMPILER} -dumpversion
-		OUTPUT_VARIABLE CLANG_VERSION)
-	if (CLANG_VERSION VERSION_LESS 3.8)
+	if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.1)
 		message(WARNING "Your clang version may not use Address-Sanitizer")
 	endif()
 	set(LRCX_COMPILER_FLAGS "-fsanitize=address -fno-omit-frame-pointer ${LRCX_COMPILER_FLAGS}")
